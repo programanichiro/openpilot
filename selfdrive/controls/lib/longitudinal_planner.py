4f4016a084d552362468d87ba77e3d26a82b1249
川岡一郎Max <>
2022-03-09 13:48:37 +0900

    エラー調査。

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -268,8 +268,8 @@ class Planner:
     #  v_cruise = self.v_desired / 2 #低速急ハンドルで速度を落とす実験
     #  with open('./cruise_info.txt','w') as fp:
     #    fp.write('%d.' % (int(v_cruise * 3.6)))
-    with open('./debug_out_vd','w') as fp:
-      fp.write('vc:%.2f[m/s] , vd:%.2f[km/h] ; ang:%.2f[deg] ; v:%.2f[km/h]' % (v_cruise * 3.6 , self.v_desired* 3.6,steerAng , v_ego * 3.6) )
+    #with open('./debug_out_vd','w') as fp:
+    #  fp.write('vc:%.2f[m/s] , vd:%.2f[km/h] ; ang:%.2f[deg] ; v:%.2f[km/h]' % (v_cruise * 3.6 , self.v_desired* 3.6,steerAng , v_ego * 3.6) )
 
     accel_limits = [A_CRUISE_MIN, get_max_accel(v_ego)]
     #accel_limits_turns = limit_accel_in_turns(v_ego, sm['carState'].steeringAngleDeg, accel_limits, self.CP)
