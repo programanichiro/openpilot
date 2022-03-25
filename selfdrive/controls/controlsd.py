6623dc327823f7b1178473eade9d515c6d727249
川岡一郎Max <>
2022-03-25 11:37:50 +0900

    エラー、これか？       STEER_CTRL_Y = self.sm['carState'].steeringAngleDeg

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -510,7 +510,7 @@ class Controls:
         self.path_xyz_stds = np.column_stack([md.position.xStd, md.position.yStd, md.position.zStd])
 
       STEER_CTRL_Y = self.sm['carState'].steeringAngleDeg
-      desired_curvature, desired_curvature_rate = get_lag_adjusted_curvature(self.CP, CS.vEgo,STEER_CTRL_Y,
+      desired_curvature, desired_curvature_rate = get_lag_adjusted_curvature(self.CP, CS.vEgo,0,
                                                                              lat_plan.psis,
                                                                              lat_plan.curvatures,
                                                                              lat_plan.curvatureRates)
