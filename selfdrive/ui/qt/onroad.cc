4b74465fab4172aa6986eee8a3fd157f3149f052
ichiroMax <>
2022-04-26 11:21:44 +0900

    TOP無指定でなんとかなりそう。配置大きさ、最終調整段階。

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -218,7 +218,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
   QWidget *btns_wrapperLL = new QWidget;
   QVBoxLayout *btns_layoutLL  = new QVBoxLayout(btns_wrapperLL);
   btns_layoutLL->setSpacing(0);
-  btns_layoutLL->setContentsMargins(30, 430, 15, 30);
+  btns_layoutLL->setContentsMargins(30+10, 430-70, 15, 30);
 
   btns_layout0L->addWidget(btns_wrapperLL,0,Qt::AlignVCenter);
   {
@@ -229,7 +229,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
       uiState()->scene.mStartAccelPowerUpButton = !mStartAccelPowerUpButton;
     });
     startAccelPowerUpButton->setFixedWidth(150);
-    startAccelPowerUpButton->setFixedHeight(150);
+    startAccelPowerUpButton->setFixedHeight(150*1.7);
     //lockOnButton->setWindowOpacity(all_opac);
     btns_layoutLL->addSpacing(0);
     btns_layoutLL->addWidget(startAccelPowerUpButton);
