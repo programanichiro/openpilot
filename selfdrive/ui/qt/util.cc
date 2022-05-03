1245b5a71752eef31a17461f36fbb1550350fc77
ichiroMax <kawaoka@yuye.co.jp>
2022-05-03 17:05:15 +0900

    バージョン表記0.8.13.1に対応。

--- selfdrive/ui/qt/util.cc
+++ selfdrive/ui/qt/util.cc
@@ -18,7 +18,7 @@ QString getBrand() {
 }
 
 QString getBrandVersion() {
-  return getBrand() + " v" + getVersion().left(18).trimmed();
+  return getBrand() + " v" + getVersion().left(20).trimmed();
 }
 
 QString getUserAgent() {
