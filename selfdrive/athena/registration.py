b5a496e950db878a1a2144053040b101453e6453
ichiroMax <kawaoka@yuye.co.jp>
2022-05-07 10:03:46 +0900

    互換機で初回起動時の登録の際、Exception無限ループした場合の対処。

--- selfdrive/athena/registration.py
+++ selfdrive/athena/registration.py
@@ -86,6 +86,10 @@ def register(show_spinner=False) -> str:
       if time.monotonic() - start_time > 60 and show_spinner:
         spinner.update(f"registering device - serial: {serial}, IMEI: ({imei1}, {imei2})")
 
+      if time.monotonic() - start_time > 70 and show_spinner:
+        dongle_id = UNREGISTERED_DONGLE_ID #互換機でException無限ループした場合の対処
+        break
+
     if show_spinner:
       spinner.close()
 
