62f689750f5799edeede0b2bce07a513e7057835
ichiroMax <kawaoka@yuye.co.jp>
2022-05-15 16:14:37 +0900

    controlsをコピー。

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 from cereal import log
 from common.filter_simple import FirstOrderFilter
@@ -7,30 +6,19 @@ from common.realtime import DT_MDL
 from selfdrive.hardware import EON, TICI
 from selfdrive.swaglog import cloudlog
 
-STEER_SAME_DIRECTION_CT = 0
-STEER_OLD_ANGLE = 0
-STEERING_CENTER = -4.3
-DCM_FRAME = 0
-dcm_handle_ctrl = True
-if os.path.isfile('./handle_center_info.txt'):
-  with open('./handle_center_info.txt','r') as fp:
-    handle_center_info_str = fp.read()
-    if handle_center_info_str:
-      STEERING_CENTER = float(handle_center_info_str)
 
 TRAJECTORY_SIZE = 33
 # camera offset is meters from center car to camera
-# model path is in the frame of EON's camera. TICI is 0.1 m away,
-# however the average measured path difference is 0.04 m
+# model path is in the frame of the camera. Empirically 
+# the model knows the difference between TICI and EON
+# so a path offset is not needed
+PATH_OFFSET = 0.00
 if EON:
-  CAMERA_OFFSET = -0.05 #0.06
-  PATH_OFFSET = 0.0
+  CAMERA_OFFSET = -0.06
 elif TICI:
   CAMERA_OFFSET = 0.04
-  PATH_OFFSET = 0.04
 else:
   CAMERA_OFFSET = 0.0
-  PATH_OFFSET = 0.0
 
 
 class LanePlanner:
@@ -74,7 +62,7 @@ class LanePlanner:
       self.l_lane_change_prob = desire_state[log.LateralPlan.Desire.laneChangeLeft]
       self.r_lane_change_prob = desire_state[log.LateralPlan.Desire.laneChangeRight]
 
-  def get_d_path(self, st_angle, pred_angle , v_ego, path_t, path_xyz):
+  def get_d_path(self, v_ego, path_t, path_xyz):
     # Reduce reliance on lanelines that are too far apart or
     # will be in a few seconds
     path_xyz[:, 1] += self.path_offset
@@ -102,21 +90,9 @@ class LanePlanner:
     self.lane_width = self.lane_width_certainty.x * self.lane_width_estimate.x + \
                       (1 - self.lane_width_certainty.x) * speed_lane_width
 
-    clipped_lane_width = min(3.5, self.lane_width) #4.0
+    clipped_lane_width = min(4.0, self.lane_width)
     path_from_left_lane = self.lll_y + clipped_lane_width / 2.0
     path_from_right_lane = self.rll_y - clipped_lane_width / 2.0
-
-    prob_limit_angle = 6 #これよりハンドル角が大きい時で、カーブのアウト側がインより薄い認識だと、アウト側を無視してみる
-    if st_angle < -prob_limit_angle:
-      if r_prob > 0.5 and r_prob*0.8 > l_prob:
-        l_prob = 0
-    elif st_angle > prob_limit_angle:
-      if l_prob > 0.5 and l_prob*0.8 > r_prob:
-        r_prob = 0
-    dcm = self.calc_dcm(st_angle, pred_angle , v_ego,clipped_lane_width,l_prob,r_prob)
-    path_from_left_lane -= dcm
-    path_from_right_lane -= dcm
-    path_xyz[:,1] -= dcm
 
     self.d_prob = l_prob + r_prob - l_prob * r_prob
     lane_path_y = (l_prob * path_from_left_lane + r_prob * path_from_right_lane) / (l_prob + r_prob + 0.0001)
@@ -127,80 +103,3 @@ class LanePlanner:
     else:
       cloudlog.warning("Lateral mpc - NaNs in laneline times, ignoring")
     return path_xyz
-
-#関数を最後に追加,dcm(ダイナミックカメラマージン？)名前がおかしいが、コーナーのイン側に寄せるオフセットである。
-  def calc_dcm(self, st_angle, pred_angle , v_ego,clipped_lane_width,l_prob,r_prob):
-    #数値を実際に取得して、調整してみる。UIスイッチで車体寄せをやめるなら、ここでゼロを返せばいい。
-    global DCM_FRAME , dcm_handle_ctrl
-    if DCM_FRAME % 30 == 1:
-      if os.path.isfile('./handle_ctrl_disable.txt'):
-        with open('./handle_ctrl_disable.txt','r') as fp:
-          dcm_handle_ctrl_disable_str = fp.read()
-          if dcm_handle_ctrl_disable_str:
-            dcm_handle_ctrl_disable = int(dcm_handle_ctrl_disable_str)
-            if dcm_handle_ctrl_disable == 0:
-              dcm_handle_ctrl = True
-            else:
-              dcm_handle_ctrl = False
-      else:
-        dcm_handle_ctrl = True
-    DCM_FRAME += 1
-    if dcm_handle_ctrl == False:
-      return 0 #車体寄せを行わない
-
-    handle_margin = 1 #1.5
-    handle_over = 10
-    camera_margin = 0.1 #0.05 -> 0.1
-    dcm = 0
-    mdcm = 1.2
-    w_add = 0
-    global STEER_SAME_DIRECTION_CT
-    global STEER_OLD_ANGLE
-    if (STEER_OLD_ANGLE) * (st_angle) > 0:
-      STEER_SAME_DIRECTION_CT += 1
-    else:
-      STEER_SAME_DIRECTION_CT = 0
-    STEER_OLD_ANGLE = st_angle
-    if v_ego > 60/3.6: # 60 or 70km/h over
-      handle_margin = 1.5
-      if STEER_SAME_DIRECTION_CT > 70 and clipped_lane_width - 2.5 >= 0:  #2.5 <- 1.9=prius width
-        w_add = (clipped_lane_width - 2.5)  * 0.8 / 2.0
-    if st_angle > handle_margin:
-      dcm = 0.01 - CAMERA_OFFSET + camera_margin
-      dcm += w_add * 1.1 / 1.2
-      dcm *= min((st_angle -(handle_margin)) / handle_over,1.2)
-    if st_angle < -handle_margin:
-      dcm = -0.11 - CAMERA_OFFSET - camera_margin
-      dcm -= w_add * 0.8 / 1.2 #減速と合わせると相当寄りすぎなので小さく
-      dcm *= min(-(st_angle +(handle_margin)) / handle_over,1.2)
-#🟥🟥🟥🟥🟥🟥🟥
-    if False: #デバッグ表示なし。
-      ms = "O:%+.2f" % (dcm)
-      if dcm >= 0.01:
-        ms+= "<"
-        for vml in range(int(min(dcm*100-1,30))):
-          ms+= "-"
-      if clipped_lane_width >= 2.5:
-        for vml in range(int(min((clipped_lane_width-2.5)*50,50))):
-          ms+= "="
-      if dcm <= -0.01:
-        for vml in range(int(min(-dcm*100-1,30))):
-          ms+= "-"
-        ms+= ">"
-      ms += "W:%.2f" % (clipped_lane_width)
-      ms += ",ct:%d;%.2f,%.2f" % (min(STEER_SAME_DIRECTION_CT,99),l_prob,r_prob)
-      #with open('./debug_out_2','w') as fp:
-      #  #fp.write('l:{0}\n'.format(['%0.2f' % i for i in path_from_left_lane]))
-      #  #fp.write('r:{0}\n'.format(['%0.2f' % i for i in path_from_right_lane]))
-      #  #fp.write('ofst:%0.2f[m] , lane_w:%0.2f[m], ct:%d' % (dcm , clipped_lane_width,STEER_SAME_DIRECTION_CT))
-      #  #fp.write('OFS:%+.2f,w:%.2f[m],ct:%d' % (dcm , clipped_lane_width,min(STEER_SAME_DIRECTION_CT,99)))
-      #  fp.write(ms)
-    #if self.camera_offset * CAMERA_OFFSET < 0: #Consider wide_cameraこれ不要。ワイドカメラがメインカメラの反対についているだけで、方向が反対になるわけではない。
-    #  dcm = -dcm
-#    if r_prob == -1 and l_prob == -1: #ない方がいいかもしれん。取ると車体が右による？。想定と逆
-#      dcm -= self.camera_offset #レーンレスモデル用のカメラオフセット反映値
-    dcm_k = abs(pred_angle)
-    if dcm_k > 10:
-      dcm_k = 10
-    dcm_k /= 10
-    return dcm * dcm_k #前方推論舵角だけ最大10度でdcmの大きさをリミットつける
