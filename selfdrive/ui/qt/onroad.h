a31ed8d0f07e000161a7a7f570f62ac93f28fe93
ichiroMax <kawaoka@yuye.co.jp>
2022-05-22 19:21:44 +0900

    リンクエラー修正。

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -79,7 +79,6 @@ private:
   void drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity , float ang);
   void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
   void drawText(QPainter &p, int x, int y, const QString &text, const QColor &col);
-  void paintEvent(QPaintEvent *event) override;
 
   QPixmap engage_img;
   QPixmap dm_img;
