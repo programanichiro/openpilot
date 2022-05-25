507f8ee512e79d1e2fbd9faefbeaff9755247da2
ichiroMax <kawaoka@yuye.co.jp>
2022-05-25 15:15:38 +0900

    0.8.14にS&G、先頭ブレーキホールド、EtsyスマートDSU対応（連絡くれた人だけ）追加。

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -20,6 +20,7 @@ from selfdrive.controls.lib.alertmanager import set_offroad_alert
 from selfdrive.hardware import HARDWARE, TICI
 from selfdrive.loggerd.config import get_available_percent
 from selfdrive.statsd import statlog
+from selfdrive.athena.registration import UNREGISTERED_DONGLE_ID
 from selfdrive.swaglog import cloudlog
 from selfdrive.thermald.power_monitoring import PowerMonitoring
 from selfdrive.thermald.fan_controller import TiciFanController
@@ -266,9 +267,10 @@ def thermald_thread(end_event, hw_queue):
     # **** starting logic ****
 
     # Ensure date/time are valid
-    now = datetime.datetime.utcnow()
-    startup_conditions["time_valid"] = (now.year > 2020) or (now.year == 2020 and now.month >= 10)
-    set_offroad_alert_if_changed("Offroad_InvalidTime", (not startup_conditions["time_valid"]))
+    if os.environ['DONGLE_ID'] != UNREGISTERED_DONGLE_ID:
+      now = datetime.datetime.utcnow()
+      startup_conditions["time_valid"] = (now.year > 2020) or (now.year == 2020 and now.month >= 10)
+      set_offroad_alert_if_changed("Offroad_InvalidTime", (not startup_conditions["time_valid"]))
 
     startup_conditions["up_to_date"] = params.get("Offroad_ConnectivityNeeded") is None or params.get_bool("DisableUpdates") or params.get_bool("SnoozeUpdate")
     startup_conditions["not_uninstalling"] = not params.get_bool("DoUninstall")
