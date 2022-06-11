5484218e65298f0876b6927b7dd90c1dc8213e95
ichiroMax <cxk01765@nifty.com>
2022-06-11 15:21:20 +0900

    ヘッダ上げ忘れ

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -79,7 +79,7 @@ public:
 
 private:
   void drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity , float ang);
-  void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
+  void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255 , bool brakeLight = false);
   void drawText(QPainter &p, int x, int y, const QString &text, const QColor &col);
 
   QPixmap engage_img;
