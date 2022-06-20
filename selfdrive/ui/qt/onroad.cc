3822ca64c0eb134bb468850790a46ad0378d8c38
ichiroMax <cxk01765@nifty.com>
2022-06-20 09:39:48 +0900

    高さ調整

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -941,7 +941,7 @@ void NvgWindow::knightScanner(QPainter &p) {
   UIState *s = uiState();
   bool left_blinker = (*s->sm)["carState"].getCarState().getLeftBlinker();
   bool right_blinker = (*s->sm)["carState"].getCarState().getRightBlinker();
-  int lane_change_height = 800; //↓の下の尖りがウインカーの底辺になるように調整。
+  int lane_change_height = 320; //↓の下の尖りがウインカーの底辺になるように調整。
   if(left_blinker || right_blinker){
     if(left_blinker == true){
       dir0 = -fabs(dir0);
@@ -952,7 +952,7 @@ void NvgWindow::knightScanner(QPainter &p) {
     hh = ww;
     hh = hh * 2 / 3;
     if((*s->sm)["carState"].getCarState().getVEgo() >= 50/3.6){
-      lane_change_height = 800;
+      lane_change_height = 320;
     }
   }
   //bool hazard_flashers = left_blinker && right_blinker; //これはtrueにならない。ハザードではleft_blinkerとright_blinkerがfalseのようだ。
