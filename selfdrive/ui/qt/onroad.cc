42c7ab101a111dc28383795bbbf19c12ae3edb39
ichiroMax <cxk01765@nifty.com>
2022-09-06 15:21:57 +0900

    ハンドルアイコンに被せて透明Longボタン配置テスト。

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -409,9 +409,23 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
   QWidget *btns_wrapper0U = new QWidget;
   QHBoxLayout *btns_layout0U  = new QHBoxLayout(btns_wrapper0U);
   btns_layout0U->setSpacing(0);
-  btns_layout0U->setContentsMargins(0, 430, 0, 0);
+  btns_layout0U->setContentsMargins(0, 430-200, 0, 0);
   btns_layout00->addWidget(btns_wrapper0U, 0, /*Qt::AlignTop |*/ Qt::AlignTop);
 
+  {
+    // Long enable 透明button
+    QPushButton *LongEnablrButton = new QPushButton("L"); //表示文字も無し。
+    QObject::connect(LongEnablrButton, &QPushButton::pressed, [=]() {
+    });
+    int rect_width = 200;
+    int rect_height = 200;
+    LongEnablrButton->setFixedWidth(rect_width);
+    LongEnablrButton->setFixedHeight(rect_height);
+    btns_layout0U->addSpacing(0);
+    btns_layout0U->addWidget(LongEnablrButton , 0 , Qt::AlignRight);
+    //forceOnePedalButton->setStyleSheet(QString(btn_style0).arg("#909090")); //線の色はダミー。
+    LongEnablrButton->setStyleSheet(QString(btn_style).arg("#909090")); //線の色はダミー。
+  }
 
   QWidget *btns_wrapper0 = new QWidget;
   QHBoxLayout *btns_layout0  = new QHBoxLayout(btns_wrapper0);
