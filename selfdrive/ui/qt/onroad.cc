17c947cefec7a47d3c6aa56c73203a566d715692
ichiroMax <cxk01765@nifty.com>
2022-09-06 15:50:52 +0900

    これでどう？

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -410,14 +410,14 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
   QHBoxLayout *btns_layout0U  = new QHBoxLayout(btns_wrapper0U);
   btns_layout0U->setSpacing(0);
   btns_layout0U->setContentsMargins(0, 430-200, 0, 0);
-  btns_layout00->addWidget(btns_wrapper0U, 0, /*Qt::AlignTop |*/ Qt::AlignVCenter);
+  btns_layout00->addWidget(btns_wrapper0U, 0, /*Qt::AlignTop |*/ Qt::AlignRight);
 
   {
     QWidget *btns_wrapperUU = new QWidget;
     QHBoxLayout *btns_layoutUU  = new QHBoxLayout(btns_wrapperUU);
     btns_layoutUU->setSpacing(0);
     btns_layoutUU->setContentsMargins(0, 0, 0, 0);
-    btns_layout0U->addWidget(btns_wrapperUU, 0, /*Qt::AlignTop |*/ Qt::AlignRight);
+    btns_layout0U->addWidget(btns_wrapperUU, 0, /*Qt::AlignTop |*/ Qt::AlignTop);
 
     {
       // Long enable 透明button
@@ -439,7 +439,7 @@ ButtonsWindow::ButtonsWindow(QWidget *parent) : QWidget(parent) {
   QHBoxLayout *btns_layout0  = new QHBoxLayout(btns_wrapper0);
   btns_layout0->setSpacing(0);
   btns_layout0->setContentsMargins(0, 0, 0, 0);
-  btns_layout0U->addWidget(btns_wrapper0, 0, /*Qt::AlignTop |*/ Qt::AlignRight);
+  btns_layout0U->addWidget(btns_wrapper0, 0, /*Qt::AlignTop |*/ Qt::AlignTop);
 
   QWidget *btns_wrapperL = new QWidget;
   QVBoxLayout *btns_layoutL  = new QVBoxLayout(btns_wrapperL);
