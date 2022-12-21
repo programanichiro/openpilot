bfe5fde566e59542be4cc0a19d733b0be1669ccb
ichiroMax <cxk01765@nifty.com>
2022-12-21 21:00:50 +0900

    ../run_auto_lock.txtにロックするスピードをテキストで30みたいに書いておく。ファイルが無いか0でオートロック無し。

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -38,6 +38,14 @@ class CarController:
 
     self.now_gear = car.CarState.GearShifter.park
     self.lock_flag = False
+    self.lock_speed = 0
+    try:
+      with open('../../../run_auto_lock.txt','r') as fp:
+        lock_speed_str = fp.read() #ロックするスピードをテキストで30みたいに書いておく。ファイルが無いか0でオートロック無し。
+        if lock_speed_str:
+          self.lock_speed = int(lock_speed_str);
+    except Exception as e:
+      pass
 
   def update(self, CC, CS):
     actuators = CC.actuators
@@ -101,12 +109,12 @@ class CarController:
 
     can_sends = []
 
-    if True: #auto door lock , unlock
+    if self.lock_speed > 0: #auto door lock , unlock
       gear = CS.out.gearShifter
       if self.now_gear != gear or CS.out.doorOpen: #ギアが変わるか、ドアが開くか。
         #アンロックしないcan_sends.append(make_can_msg(0x750, b'\x40\x05\x30\x11\x00\x40\x00\x00', 0)) #auto unlock
         self.lock_flag = False #ドアが空いてもフラグはおろす。
-      elif gear == car.CarState.GearShifter.drive and self.lock_flag == False and CS.out.vEgo >= 30/3.6: #時速30km/h以上でオートロック
+      elif gear == car.CarState.GearShifter.drive and self.lock_flag == False and CS.out.vEgo >= self.lock_speed/3.6: #時速30km/h以上でオートロック
         can_sends.append(make_can_msg(0x750, b'\x40\x05\x30\x11\x00\x80\x00\x00', 0)) #auto lock
         self.lock_flag = True
       self.now_gear = gear
