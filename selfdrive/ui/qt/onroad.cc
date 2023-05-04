439ad9d87e72568e80f5888af3f1ccdf0e0a4cca
ichiroMax <cxk01765@nifty.com>
2023-05-04 09:35:59 +0900

    コードミス修正。

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -1493,7 +1493,7 @@ void AnnotatedCameraWidget::drawHud(QPainter &p) {
     pen.setCapStyle(Qt::FlatCap); //端をフラットに
     p.setPen(pen);
 
-    p.drawArc(traffic_speed_x-arc_w/2+4, traffic_speed_y-arc_w/2+4, traffic_speed_r*2+arc_w-10,traffic_speed_r*2+arc_w-8, 0*16, 360*8);
+    p.drawArc(traffic_speed_x-arc_w/2+4, traffic_speed_y-arc_w/2+4, traffic_speed_r*2+arc_w-8,traffic_speed_r*2+arc_w-8, 0*16, 360*16);
     int f_size = traffic_speed_r * 67 / (150 / 2);
     configFont(p, "Inter",f_size , "Bold");
     drawText(p, traffic_speed_x+traffic_speed_r, traffic_speed_y+traffic_speed_r+f_size/2 -7, traffic_speed , QColor(0x24, 0x57, 0xa1 , 255));
