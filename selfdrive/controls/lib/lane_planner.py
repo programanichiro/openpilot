af2fd40e83e76f05ddbe4beb6ff48a577d283d04
ichiroMax <cxk01765@nifty.com>
2023-05-30 18:33:10 +0900

    diff_add = 0.05 #さらに押し戻す距離[m]をロジック追加。

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -96,6 +96,7 @@ class LanePlanner:
     # clipped_lane_width = min(4.0, self.lane_width)
     # path_from_left_lane = self.lll_y + clipped_lane_width / 2.0
     # path_from_right_lane = self.rll_y - clipped_lane_width / 2.0
+    #速度によってマージンを増したほうがいい？
     path_from_left_lane = self.lll_y + 1.8 / 2.0 + 0.3 + 0.05 #プリウスの車幅だけ補正して、左端〜右端の間はe2eの推論選択に任せる。
     path_from_right_lane = self.rll_y - 1.8 / 2.0 - 0.3
 
@@ -150,6 +151,8 @@ class LanePlanner:
       #         lock_off = True #ロックオンOFFで右レーン依存テスト
       # except Exception as e:
       #   pass
+      diff_mul = 1.1 #押し戻すための倍率
+      diff_add = 0.05 #さらに押し戻す距離[m]
       if False: #lock_off == True:
         diff_r = lane_path_y_interp_right[0] - path_xyz[:,1][0]
         path_xyz[:,1] += diff_r #lane_path_y_interp_rightのカーブ形状が使えないとなると、path_xyzを活かさなければならない。
@@ -160,13 +163,13 @@ class LanePlanner:
           # path_xyz[:,1] = [min(a, b) for a, b in zip(lane_path_y_interp_right, path_xyz[:,1])]
           diff_r = lane_path_y_interp_right[0] - path_xyz[:,1][0]
           if diff_r < 0:
-            path_xyz[:,1] += diff_r * 1.1 #lane_path_y_interp_rightのカーブ形状が使えないとなると、path_xyzを活かさなければならない。
+            path_xyz[:,1] += diff_r * diff_mul -diff_add #lane_path_y_interp_rightのカーブ形状が使えないとなると、path_xyzを活かさなければならない。
             new_lane_collision |= 2
         if l_prob > 0.5: #レーン左からはみ出さないように。
           # path_xyz[:,1] = [max(a, b) for a, b in zip(lane_path_y_interp_left, path_xyz[:,1])]
           diff_l = lane_path_y_interp_left[0] - path_xyz[:,1][0]
           if diff_l > 0:
-            path_xyz[:,1] += diff_l * 1.1 #lane_path_y_interp_rightのカーブ形状が使えないとなると、path_xyzを活かさなければならない。
+            path_xyz[:,1] += diff_l * diff_mul +diff_add #lane_path_y_interp_leftのカーブ形状が使えないとなると、path_xyzを活かさなければならない。
             new_lane_collision |= 1
       else:
         #右に曲がる時は左->右の順番で検査する。カーブの内側に切り込まないように。
@@ -174,13 +177,13 @@ class LanePlanner:
           # path_xyz[:,1] = [max(a, b) for a, b in zip(lane_path_y_interp_left, path_xyz[:,1])]
           diff_l = lane_path_y_interp_left[0] - path_xyz[:,1][0]
           if diff_l > 0:
-            path_xyz[:,1] += diff_l * 1.1 #lane_path_y_interp_rightのカーブ形状が使えないとなると、path_xyzを活かさなければならない。
+            path_xyz[:,1] += diff_l * diff_mul +diff_add #lane_path_y_interp_leftのカーブ形状が使えないとなると、path_xyzを活かさなければならない。
             new_lane_collision |= 1
         if r_prob > 0.5: #レーン右からはみ出さないように。
           # path_xyz[:,1] = [min(a, b) for a, b in zip(lane_path_y_interp_right, path_xyz[:,1])]
           diff_r = lane_path_y_interp_right[0] - path_xyz[:,1][0]
           if diff_r < 0:
-            path_xyz[:,1] += diff_r * 1.1 #lane_path_y_interp_rightのカーブ形状が使えないとなると、path_xyzを活かさなければならない。
+            path_xyz[:,1] += diff_r * diff_mul -diff_add #lane_path_y_interp_rightのカーブ形状が使えないとなると、path_xyzを活かさなければならない。
             new_lane_collision |= 2
     else:
       # cloudlog.warning("Lateral mpc - NaNs in laneline times, ignoring")
