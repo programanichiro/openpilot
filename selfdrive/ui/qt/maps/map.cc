811b15a048f0b07e40e8b118b19f26ff370ab4b4
ichiroMax <cxk01765@nifty.com>
2023-06-06 11:38:24 +0900

    ファイル名に合わせて、変数名をlimitspeed_data_txtに修正。

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -855,12 +855,12 @@ int limit_speed_num;
 
 void MapLimitspeed::updateLimitspeed(int map_width) {
 
-  std::string limitspeed_info_txt = util::read_file("/tmp/limitspeed_data.txt");
+  std::string limitspeed_data_txt = util::read_file("/tmp/limitspeed_data.txt");
   if(limitspeed_info_txt.empty() == false){
     float output[3]; // float型の配列
     int i = 0; // インデックス
 
-    std::stringstream ss(limitspeed_info_txt); // 入力文字列をstringstreamに変換
+    std::stringstream ss(limitspeed_data_txt); // 入力文字列をstringstreamに変換
     std::string token; // 一時的にトークンを格納する変数
     while (std::getline(ss, token, ',') && i < 3) { // カンマで分割し、一つずつ処理する
       output[i] = std::stof(token); // 分割された文字列をfloat型に変換して配列に格納
