e65f93800bf7aa0aebb8e04c4ea6b9121d18a1e3
ichiroMax <cxk01765@nifty.com>
2023-06-07 10:42:02 +0900

    これでどう？

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -45,7 +45,7 @@ def manager_init() -> None:
     ("HasAcceptedTerms", "0"),
     ("LanguageSetting", "main_en"),
     ("OpenpilotEnabledToggle", "1"),
-    ("LongitudinalPersonality", str(log.LongitudinalPersonality.standard)),
+    #("LongitudinalPersonality", str(log.LongitudinalPersonality.standard)),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
