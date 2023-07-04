c43ad999e5e83459226187abc1007a6a1a223966
ichiroMax <cxk01765@nifty.com>
2023-07-04 10:57:08 +0900

    標識高さ計算ミス修正。

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -933,9 +933,9 @@ void MapLimitspeed::updateLimitspeed(int map_width) {
   }
 #endif
   if (map_width == 0 || (false && uiState()->scene.map_on_left)) {
-    this->move(30, 1080 - r*2 - bdr_s - stand_still_height); //地図にナビ用ボタンが追加されたので、こちらは使わない。
+    this->move(30, 1080 - bdr_s*2 - r*2 - bdr_s - stand_still_height); //地図にナビ用ボタンが追加されたので、こちらは使わない。
   } else {
-    this->move(map_width - r*2 - bdr_s, 1080 - r*2 - bdr_s - stand_still_height);
+    this->move(map_width - r*2 - bdr_s, 1080 - bdr_s*2 - r*2 - bdr_s - stand_still_height);
   }
 }
 
