688c300f78cfc245132493a87f84b6cd6a9fa633
ichiroMax <cxk01765@nifty.com>
2023-11-12 08:57:20 +0900

    sol,1への修正は無しでテスト。おそらく意味がない気がする。

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -103,10 +103,10 @@ class LateralPlanner:
       STEER_CTRL_Y -= handle_center #STEER_CTRL_Yにhandle_centerを込みにする。
       self.path_xyz , lane_d = self.LP.get_d_path(STEER_CTRL_Y , (-max_yp / 2.5) , ypf , self.v_ego, self.t_idxs, self.path_xyz)
       if len(md.position.x) == TRAJECTORY_SIZE and len(md.velocity.x) == TRAJECTORY_SIZE and len(md.lateralPlannerSolution.x) == TRAJECTORY_SIZE:
-        self.x_sol[:,1] -= lane_d * 0.15 #変更したパスを維持するため？
+        # self.x_sol[:,1] -= lane_d * 0.15 #変更したパスを維持するため？
         self.x_sol[:,2] += lane_d * 0.02 #yaw（ハンドル制御の元値）をレーンの反対へ戻す
         with open('/tmp/debug_out_v','w') as fp:
-          fp.write("%+.3f/%+.3f , %+.3f/%+.3f" % (lane_d * 0.02 ,self.x_sol[CONTROL_N,2],-lane_d * 0.15,self.x_sol[CONTROL_N,1]))
+          fp.write("%+.3f/%+.3f , %+.3f/%+.3f" % (lane_d ,self.x_sol[CONTROL_N-1,2],-lane_d,self.x_sol[CONTROL_N-1,1]))
         # self.x_sol[:,0] *= 0 #レーン表示の縦が伸びない。しかし表示だけ
         # self.x_sol[:,1] *= 0 #パス表示が直進になる。しかし表示だけ
         # self.x_sol[:,2] *= 0 #yawを無効に。ハンドル制御できる確認。
