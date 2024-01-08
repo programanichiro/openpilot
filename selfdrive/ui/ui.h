5840e015b42a629a38836956a84c44e8a8ce79ec
ichiroMax <cxk01765@nifty.com>
2024-01-08 15:36:03 +0900

    かけるとレーンが狭まる。÷0.92とする。

--- selfdrive/ui/ui.h
+++ selfdrive/ui/ui.h
@@ -32,7 +32,7 @@ constexpr mat3 FCAM_INTRINSIC_MATRIX = (mat3){{2648.0, 0.0, 1928.0 / 2,
                                            0.0, 0.0, 1.0}};
 // tici ecam focal probably wrong? magnification is not consistent across frame
 // Need to retrain model before this can be changed
-constexpr mat3 ECAM_INTRINSIC_MATRIX = (mat3){{567.0 * 0.5, 0.0, 1928.0 / 2 * 0.92, //FCAM_INTRINSIC_MATRIXより横に間伸びしているのを調整する。
+constexpr mat3 ECAM_INTRINSIC_MATRIX = (mat3){{567.0 / 0.92, 0.0, 1928.0 / 2 * 0.92, //FCAM_INTRINSIC_MATRIXより横に間伸びしているのを調整する。
                                            0.0, 567.0, 1208.0 / 2,
                                            0.0, 0.0, 1.0}};
 
