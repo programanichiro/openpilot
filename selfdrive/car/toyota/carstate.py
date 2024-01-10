51d4b62e31f5ebf54ec1faf3242a89a685a42171
ichiroMax <cxk01765@nifty.com>
2024-01-10 15:34:57 +0900

    0.1未満角度エミュレータ自分だけ。

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -87,29 +87,30 @@ class CarState(CarStateBase):
     ret.standstill = ret.vEgoRaw == 0
 
     ret.steeringAngleDeg = cp.vl["STEER_ANGLE_SENSOR"]["STEER_ANGLE"] + cp.vl["STEER_ANGLE_SENSOR"]["STEER_FRACTION"]
-    steeringAngleDeg0 = ret.steeringAngleDeg
-    self.steeringAngleDegs.append(float(steeringAngleDeg0))
-    angV = 0
-    #angA = 0
-    if len(self.steeringAngleDegs) > 17:
-      self.steeringAngleDegs.pop(0)
-      # 過去19フレーム(0.19秒)の角度から、角速度と角加速度の平均を求める。
-      angVs = [self.steeringAngleDegs[i + 1] - self.steeringAngleDegs[i] for i in range(len(self.steeringAngleDegs) - 1)] #過去９回の角速度
-      #angAs = [angVs[i + 1] - angVs[i] for i in range(len(angVs) - 1)] #過去８回の角加速度
-      angV = sum(angVs) / len(angVs)
-      #angA = sum(angAs) / len(angAs)
-      self.prob_ang += angV
+    if self.flag_47700:
+      steeringAngleDeg0 = ret.steeringAngleDeg
+      self.steeringAngleDegs.append(float(steeringAngleDeg0))
+      angV = 0
+      #angA = 0
+      if len(self.steeringAngleDegs) > 17:
+        self.steeringAngleDegs.pop(0)
+        # 過去17フレーム(0.17秒)の角度から、角速度と角加速度の平均を求める。
+        angVs = [self.steeringAngleDegs[i + 1] - self.steeringAngleDegs[i] for i in range(len(self.steeringAngleDegs) - 1)] #過去９回の角速度
+        #angAs = [angVs[i + 1] - angVs[i] for i in range(len(angVs) - 1)] #過去８回の角加速度
+        angV = sum(angVs) / len(angVs)
+        #angA = sum(angAs) / len(angAs)
+        self.prob_ang += angV
 
-    if self.before_ang != ret.steeringAngleDeg:
-      self.before_ang_ct = 0
-      self.prob_ang = 0
-    else:
-      self.before_ang_ct += 1
-    # prob_ang = self.before_ang_ct * angV + (self.before_ang_ct-1) * self.before_ang_ct / 2 * angA
-    self.before_ang = ret.steeringAngleDeg
-    with open('/tmp/debug_out_v','w') as fp:
-      fp.write("ct:%d,%+.2f,%+.2f,%+.2f" % (self.before_ang_ct,ret.steeringAngleDeg,ret.steeringAngleDeg+self.prob_ang,angV))
-    ret.steeringAngleDeg += self.prob_ang
+      if self.before_ang != ret.steeringAngleDeg:
+        self.before_ang_ct = 0
+        self.prob_ang = 0
+      else:
+        self.before_ang_ct += 1
+      # prob_ang = self.before_ang_ct * angV + (self.before_ang_ct-1) * self.before_ang_ct / 2 * angA
+      self.before_ang = ret.steeringAngleDeg
+      # with open('/tmp/debug_out_v','w') as fp:
+      #   fp.write("ct:%d,%+.2f,%+.2f,%+.2f" % (self.before_ang_ct,ret.steeringAngleDeg,ret.steeringAngleDeg+self.prob_ang,angV))
+      ret.steeringAngleDeg += self.prob_ang
     ret.steeringRateDeg = cp.vl["STEER_ANGLE_SENSOR"]["STEER_RATE"]
     torque_sensor_angle_deg = cp.vl["STEER_TORQUE_SENSOR"]["STEER_ANGLE"]
 
