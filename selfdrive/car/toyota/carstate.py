8baff23aa604996442fa618feac1498dce69ad9f
ichiroMax <cxk01765@nifty.com>
2024-01-14 09:45:19 +0900

    self.accurate_steer_angle_seenをdebug_out_oに書き出す。

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -104,6 +104,8 @@ class CarState(CarStateBase):
     # On some cars, the angle measurement is non-zero while initializing
     if abs(torque_sensor_angle_deg) > 1e-3 and not bool(cp.vl["STEER_TORQUE_SENSOR"]["STEER_ANGLE_INITIALIZING"]):
       self.accurate_steer_angle_seen = (not self.flag_47700) if (ret.knight_scanner_bit3 & 0x04) else True #True , 自分だけFalseにする, ただし knight_scanner_bit3.txt ⚪︎⚪︎⚫︎を切ると常にTrue
+      with open('/tmp/debug_out_o','w') as fp:
+        fp.write("%d" % (self.accurate_steer_angle_seen))
 
     if self.accurate_steer_angle_seen:
       # Offset seems to be invalid for large steering angles and high angle rates
