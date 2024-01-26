654b3dd30e02c4490d8844c3bf7f69a86725af41
ichiroMax <cxk01765@nifty.com>
2024-01-26 18:19:30 +0900

    longitudinalTuning一部Cydiaチューン取り込み（TSS2縦制御用）

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -266,15 +266,18 @@ class CarInterface(CarInterfaceBase):
 
     # on stock Toyota this is -2.5
     ret.stopAccel = -2.5
-    ret.stoppingDecelRate = 0.1
 
     tune = ret.longitudinalTuning
     tune.deadzoneBP = [0., 9.]
     tune.deadzoneV = [.0, .15]
-    ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
+    ret.stoppingDecelRate = 0.3  # This is okay for TSS-P
+    if candidate in TSS2_CAR:
+      ret.vEgoStopping = 0.25
+      ret.vEgoStarting = 0.25
+      ret.stoppingDecelRate = 0.009  # reach stopping target smoothly
     tune.kpBP = [0.,]
-    tune.kiBP = [0., 3.]
     tune.kpV = [1.]
+    tune.kiBP = [0., 3.]
     tune.kiV = [.3, 1.]
 
     return ret
