c7890ad2f86d458c58ef0c050d9e0cb24d062118
ichiroMax <cxk01765@nifty.com>
2024-02-07 19:52:34 +0900

    下限速度取得ミス。

--- selfdrive/ui/qt/home.cc
+++ selfdrive/ui/qt/home.cc
@@ -68,7 +68,7 @@ void HomeWindow::updateState(const UIState &s) {
 
   static bool blinker_stat = false;
   uint16_t lsta = (uint16_t)(sm["modelV2"].getModelV2().getMeta().getLaneChangeState()); //enum LaneChangeState.preLaneChange == 1 , log.capnp
-  double vEgo sm["carState"].getCarState().getVEgo() * 3.6;
+  double vEgo = sm["carState"].getCarState().getVEgo() * 3.6;
   bool left_blinker = sm["carState"].getCarState().getLeftBlinker() && vEgo > 45 && lsta == 1;
   bool right_blinker = sm["carState"].getCarState().getRightBlinker() && vEgo > 45 && lsta == 1;
   bool back_gear = ((uint16_t)(sm["carState"].getCarState().getGearShifter()) == 4);//car.capnp , enum GearShifterにバックギアが定義されている。
