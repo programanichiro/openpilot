d4bd83b8d2d06a4219fd76a2f0acc11f7f31ae2d
ichiroMax <cxk01765@nifty.com>
2024-02-17 12:36:06 +0900

    ボタン色半透明。

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -58,7 +58,7 @@ private:
   // int lsStatus = -1;  // always initialize style sheet and send msg
   // const QStringList lsButtonColors = {"#ff3737", "#37b868", "#044389"};
 
-  const QStringList mButtonColors = {"#909090", "#37b868"};
+  const QStringList mButtonColors = {"#c0909090", "#c037b868"};
 
   // model long button
   bool mLockOnButton = true;  // triggers initialization
