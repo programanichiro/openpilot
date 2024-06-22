dc866f43e35b64a54b44c4b2a9ebbbb1e635f4fd
ichiroMax <cxk01765@nifty.com>
2024-06-22 16:03:30 +0900

    スタイル設定ミス。

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -660,7 +660,7 @@ void MapWindow::updateState(const UIState &s) {
       if(now_navigation == false && night_mode >= 0){
         night_mode = -1; //ナビ中の昼夜切り替えを無効にする。昼夜切り替えでルートが消えるから、この処理は必須。
         //m_map->setStyleUrl("mapbox://styles/commaai/clkqztk0f00ou01qyhsa5bzpj"); //ナビ中はスタイルを公式に戻す。
-        m_map->setStyleUrl("mapboxmapbox://styles/kawombop0/clxpnfmk000ke01pu7kk4ehrt"); //ナビ中はスタイルを公式に戻す。公式＋渋滞情報
+        m_map->setStyleUrl("mapbox://styles/kawombop0/clxpnfmk000ke01pu7kk4ehrt"); //ナビ中はスタイルを公式に戻す。公式＋渋滞情報
         style_reload = 10;
       }
       now_navigation = true;
