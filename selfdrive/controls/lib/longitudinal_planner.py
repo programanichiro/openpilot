f5f1479e7c545bf5ef66f31462f7a29476b74f33
ichiroMax <cxk01765@nifty.com>
2024-06-26 19:35:33 +0900

    青信号発進状況UI表示。

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -320,7 +320,7 @@ class LongitudinalPlanner:
     hasLead = sm['radarState'].leadOne.status
     distLead_near = sm['radarState'].leadOne.dRel < interp(v_ego*3.6 , [30,80] , [50,120]) #前走車が近ければTrue
     global signal_scan_ct,path_x_old_signal,path_x_old_signal_check , red_signal_scan_flag
-    if v_ego <= 0.01/3.6 and (OP_ENABLE_v_cruise_kph > 0 or one_pedal == False or (OP_ENABLE_v_cruise_kph == 0 and (hasLead == False or distLead_near == False))) and sm['controlsState'].enabled and sm['carState'].gasPressed == False: #and (hasLead == False or (sm['radarState'].leadOne.dRel > 40 and sm['radarState'].leadOne.modelProb > 0.5)):
+    if v_ego <= 0.1/3.6 and (OP_ENABLE_v_cruise_kph > 0 or one_pedal == False or (OP_ENABLE_v_cruise_kph == 0 and (hasLead == False or distLead_near == False))) and sm['controlsState'].enabled and sm['carState'].gasPressed == False: #and (hasLead == False or (sm['radarState'].leadOne.dRel > 40 and sm['radarState'].leadOne.modelProb > 0.5)):
       #速度ゼロでエンゲージ中、前走車なしでアクセル踏んでない。
       steer_ang = sm['carState'].steeringAngleDeg - handle_center
       # 停止時の青信号発進抑制、一時的に緩和、15->50度
@@ -328,16 +328,20 @@ class LongitudinalPlanner:
         #path_xyz = np.column_stack([md.position.x, md.position.y, md.position.z])
         path_x = md.position.x #path_xyz[:,0]
         # with open('/tmp/debug_out_k','w') as fp: #path_xの中を解析して、ビュンと伸びる瞬間を判断したい。
+        #   fp.write('x:%.2f,ct:%d,px:%.1f,v:%.1f' % (path_x[TRAJECTORY_SIZE -1],signal_scan_ct,path_x_old_signal_check,v_ego))
         #   #fp.write('{0}\n'.format(['%0.2f' % i for i in path_x]))
         #   fp.write('l:%d(%.2f),%.2f[m],x:%.2f' % (hasLead ,sm['radarState'].leadOne.modelProb , sm['radarState'].leadOne.dRel , path_x[TRAJECTORY_SIZE -1]))
-        if (path_x_old_signal < 2) and path_x[TRAJECTORY_SIZE -1] > 40:
+        with open('/tmp/blue_signal_chk.txt','w') as fp: #path_xの中を解析して、ビュンと伸びる瞬間を判断したい。
+          fp.write('%d' % (int(path_x[TRAJECTORY_SIZE -1])))
+        half_limit = 25 #40
+        if (path_x_old_signal < 2) and path_x[TRAJECTORY_SIZE -1] > half_limit:
           path_x_old_signal_check = path_x[TRAJECTORY_SIZE -1] #ゆっくり立ち上がったらこれはTrueにならない。
-        path_x_base_limit = 64.0 #70.0 , この座標値超で青信号スタート発火。
-        if path_x[TRAJECTORY_SIZE -1] > path_x_base_limit or path_x_old_signal_check > 40: #青信号判定の瞬間
+        path_x_base_limit = 30 #64.0 #70.0 , この座標値超で青信号スタート発火。
+        if path_x[TRAJECTORY_SIZE -1] > path_x_base_limit or path_x_old_signal_check > half_limit: #青信号判定の瞬間
           path_x_old_signal_check += path_x[TRAJECTORY_SIZE -1] #最初の立ち上がりは2倍される
           signal_scan_ct += 1 #横道からの進入車でパスが伸びたのを勘違いするので、バッファを設ける。
           limit_8 = 8 if path_x[TRAJECTORY_SIZE -1] > path_x_base_limit else 16
-          if signal_scan_ct > limit_8 and signal_scan_ct < 100 and (path_x[TRAJECTORY_SIZE -1] > path_x_base_limit or (path_x_old_signal_check-40) > 50*limit_8):
+          if signal_scan_ct > limit_8 and signal_scan_ct < 100 and (path_x[TRAJECTORY_SIZE -1] > path_x_base_limit or (path_x_old_signal_check-half_limit) > 1.25*limit_8 * half_limit): #path_x_old_signal_check-half_limitの20倍程度
             with open('/tmp/signal_start_prompt_info.txt','w') as fp:
               if sm['driverMonitoringState'].isActiveMode == True: #よそ見をしていたら発進しない。
                 if OP_ENABLE_v_cruise_kph > 0: #MAX==1の状態。
@@ -361,7 +365,7 @@ class LongitudinalPlanner:
         path_x_old_signal = path_x[TRAJECTORY_SIZE -1]
     else:
       signal_scan_ct = 0 if signal_scan_ct < 4 else signal_scan_ct - 4
-    if path_x_old_signal < 30:
+    if path_x_old_signal < 20:
       path_x_old_signal_check = 0
 
     if a_ego > 0 and v_ego >= min_acc_speed/3.6 and OP_ENABLE_v_cruise_kph > 0 and sm['controlsState'].enabled and sm['carState'].gas > 0.32: #アクセル強押しでワンペダルからオートパイロットへ
