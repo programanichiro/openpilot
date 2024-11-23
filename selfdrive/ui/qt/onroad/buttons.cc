b1ee242aa3d443b6a69b8ca88e9ebfdc89003b23
ichiroMax <cxk01765@nifty.com>
2024-11-23 10:43:48 +0900

    /tmpフォルダに書き込み失敗したらリトライする。

--- selfdrive/ui/qt/onroad/buttons.cc
+++ selfdrive/ui/qt/onroad/buttons.cc
@@ -61,11 +61,14 @@ void copy_manager2tmp(const char*fn_mng , const char*txt_mng , bool first){ //tx
         return;
       }
     }
-    FILE *dst = fopen(tmpfn,"w");
-    if(dst){
-      fwrite(txt_mng,strlen(txt_mng),1,dst);
-      fclose(dst);
-    }
+    do {
+      FILE *dst = fopen(tmpfn,"w");
+      if(dst){
+        fwrite(txt_mng,strlen(txt_mng),1,dst);
+        fclose(dst);
+        break;
+      }
+    } while(1);
   }
 }
 
