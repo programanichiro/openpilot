134c5cf8334491418f2b7b6c45904eef822fb759
ichiroMax <cxk01765@nifty.com>
2025-02-15 08:13:02 +0900

    gpsAllways

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -28,7 +28,7 @@ def ublox(started: bool, params: Params, CP: car.CarParams) -> bool:
   use_ublox = ublox_available()
   if use_ublox != params.get_bool("UbloxAvailable"):
     params.put_bool("UbloxAvailable", use_ublox)
-  return started and use_ublox
+  return True and use_ublox #startedをTrueに、Onroad後にGPSと捉えるのが遅いから対策。
 
 def joystick(started: bool, params: Params, CP: car.CarParams) -> bool:
   return started and params.get_bool("JoystickDebugMode")
