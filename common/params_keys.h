34c85785a5a6d218ce2350cfe1627ce60a35bad1
ichiroMax <cxk01765@nifty.com>
2025-02-20 19:35:16 +0900

    params.ccからkeyを引っ越し。

--- common/params_keys.h
+++ common/params_keys.h
@@ -76,6 +76,8 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"LocationFilterInitialState", PERSISTENT},
     {"LongitudinalManeuverMode", CLEAR_ON_MANAGER_START | CLEAR_ON_OFFROAD_TRANSITION},
     {"LongitudinalPersonality", PERSISTENT},
+    {"AccelMethodSwitch", PERSISTENT},
+    {"GpsAlwaysSwitch", PERSISTENT},
     {"NetworkMetered", PERSISTENT},
     {"ObdMultiplexingChanged", CLEAR_ON_MANAGER_START | CLEAR_ON_ONROAD_TRANSITION},
     {"ObdMultiplexingEnabled", CLEAR_ON_MANAGER_START | CLEAR_ON_ONROAD_TRANSITION},
