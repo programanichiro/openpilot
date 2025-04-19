3381954d98d5f0b844943914114c095049d6a5cb
ichiroMax <cxk01765@nifty.com>
2025-04-19 09:05:37 +0900

    実験。ワンペダル中の低速操作で常にアクセル操作をMAXに伝える。アクセルを放しても減速しなくなる。

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -318,6 +318,8 @@ class LongitudinalPlanner:
         if int(accel_engaged_str) >= 3 and sm['carState'].gasPressed == False: #ワンペダルモード(開始時にアクセル操作していたら低速エンゲージとする)
           OP_ENABLE_gas_speed = 1.0 / 3.6
       OP_ENABLE_ACCEL_RELEASE = False
+    if OP_ENABLE_v_cruise_kph != 0 and sm['carState'].gasPressed and vk_ego > 10/3.6 and vk_ego < min_acc_speed/3.6:
+      OP_ENABLE_ACCEL_RELEASE = False #ワンペダル中の低速操作で常にアクセル操作をMAXに伝える。アクセルを放しても減速しなくなる。
     if sm_longControlState != LongCtrlState.off:
       OP_ENABLE_PREV = True
       if sm['carState'].gasPressed and OP_ENABLE_ACCEL_RELEASE == False:
