d17b8e632d4f94924084b98e576d1718752417b0
ichiroMax <cxk01765@nifty.com>
2025-06-07 18:44:28 +0900

    ダブルタップは来てる。これで変わるか検証。

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -272,10 +272,11 @@ void MapWindow::initLayers() {
   }
   if (!m_map->layerExists("carPosLayer")) {
     qDebug() << "Initializing carPosLayer";
-    if(my_mapbox_triangle.empty() == false){
+    if(false && my_mapbox_triangle.empty() == false){
       m_map->addImage("label-arrow", QImage("/data/mb_triangle.svg"));
     } else {
-      m_map->addImage("label-arrow", QImage("../assets/images/triangle.svg"));
+      //m_map->addImage("label-arrow", QImage("../assets/images/triangle.svg"));
+      m_map->addImage("label-arrow", QImage("../assets/images/triangle_green.svg_"));
     }
 
     QVariantMap carPos;
@@ -930,7 +931,7 @@ void MapWindow::mouseReleaseEvent(QMouseEvent *ev) {
   { //以下はタッチが放された瞬間に記録する。
     setButtonInt("/data/mb_zoom_offset.txt",(int)(zoom_offset * 1000)); //コンパス長押しのクリアはここでは保存されない。
     if(width_rate >= 0){
-      //ダブつタップでクリアしてもここで保存される。
+      //ダブルタップでクリアしてもここで保存される。
       FILE *fp = fopen("/data/mb_width_rate.txt","w"); //write_fileだと書き込めないが、こちらは書き込めた。
       if(fp != NULL){
         fprintf(fp,"%.3f",width_rate);
@@ -1026,12 +1027,12 @@ void MapWindow::mouseReleaseEvent(QMouseEvent *ev) {
 }
 
 void MapWindow::mouseDoubleClickEvent(QMouseEvent *ev) {
-  if(interaction_counter > INTERACTION_TIMEOUT * 0.9){
+  if(false && interaction_counter > INTERACTION_TIMEOUT * 0.9){
     //移動直後の0.5秒以内のダブルクリックはリセット動作をしない。
     return;
   }
 
-  if(m_lastPos.y() < 1080 - 200 && start_window_resize == true){ //ボタンの位置は避ける。
+  if(false && m_lastPos.y() < 1080 - 200 && start_window_resize == true){ //ボタンの位置は避ける。
     bool clear_width_rate = false;
     if(uiState()->scene.map_on_left){
       if(m_lastPos.x() > this->width() - 150){
@@ -1058,11 +1059,13 @@ void MapWindow::mouseDoubleClickEvent(QMouseEvent *ev) {
   }
 
   if (last_position) m_map->setCoordinate(*last_position);
+  update();
   if(north_up == 0){
     if (last_bearing) m_map->setBearing(*last_bearing+bearing_ofs(velocity_filter.x()));
   } else {
     if (last_bearing) m_map->setBearing(0);
   }
+  update();
   m_map->setZoom(util::map_val<float>(velocity_filter.x(), 0, 30, MAX_ZOOM, MIN_ZOOM));
   update();
 
