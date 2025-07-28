d5305295e171648375e0d6f3f613201e0d8cd8f1
ichiroMax <cxk01765@nifty.com>
2025-07-28 12:41:23 +0900

    ハザードで枠をオレンジ点滅

--- selfdrive/ui/qt/onroad/onroad_home.cc
+++ selfdrive/ui/qt/onroad/onroad_home.cc
@@ -69,13 +69,21 @@ void OnroadWindow::updateState(const UIState &s) {
   mapVisible = isMapVisible();
 
   QColor bgColor = bg_colors[s.status];
-  std::string hazard_light_str = util::read_file("/tmp/hazard_light.txt");
-  if(hazard_light_str.empty() == false){
-    int hazard_light = std::stoi(hazard_light_str);
-    if(hazard_light > 0){
-      //bgColorをオレンジに点滅
-      bgColor = QColor(192, 102, 0, 255); //ウインカーと同じ色
+  static bool hazard = false;
+  static unsigned int hazard_ct = 0;
+  if((hazard_ct++ % 10) == 0){
+    std::string hazard_light_str = util::read_file("/tmp/hazard_light.txt");
+    if(hazard_light_str.empty() == false){
+      int hazard_light = std::stoi(hazard_light_str);
+      if(hazard_light > 0){
+        //bgColorをオレンジに点滅
+        hazard = !hazard;
+      }
     }
+  }
+  if(hazard == true){
+    //bgColorをオレンジに点滅
+    bgColor = QColor(192, 102, 0, 255); //ウインカーと同じ色
   }
   if (bg != bgColor) {
     // repaint border
