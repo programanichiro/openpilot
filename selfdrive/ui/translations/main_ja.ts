344c151af5988653d05aee398639f0e7272b347c
ichiroMax <cxk01765@nifty.com>
2025-08-17 19:26:12 +0900

    翻訳対応2 (+2 squashed commits) Squashed commits: [da090fc4f2] 翻訳対応。 [36b079abd9] TSSP→Recommend

--- selfdrive/ui/translations/main_ja.ts
+++ selfdrive/ui/translations/main_ja.ts
@@ -1040,8 +1040,16 @@ Firehoseモードを有効にすると学習データを最大限アップロー
         <translation>アクセル制御セレクト</translation>
     </message>
     <message>
-        <source>Switch Accel Method to Official version or Cydia2020. A reboot is required.</source>
-        <translation>アクセル制御をCydia2020氏の手法と公式から選択します。再起動が必要です。TSSPでは公式は非推奨です。</translation>
+        <source>Switch Accel Method to Official version or recommendation. A reboot is required.</source>
+        <translation>アクセル制御を推奨のの手法と公式から選択します。再起動が必要です。TSSPでは推奨を選択してください。</translation>
+    </message>
+    <message>
+        <source>Recommend</source>
+        <translation>推奨</translation>
+    </message>
+    <message>
+        <source>Official</source>
+        <translation>公式</translation>
     </message>
     <message>
         <source>Auto door lock by speed</source>
