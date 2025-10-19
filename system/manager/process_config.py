a923f03a3de021338a91d7856948ba446f5e9983
ichiroMax <cxk01765@nifty.com>
2025-10-19 09:00:01 +0900

    nomapはraylib UIのみ。今後AGNOS14に追従していく。

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -82,8 +82,8 @@ procs = [
   PythonProcess("dmonitoringmodeld", "selfdrive.modeld.dmonitoringmodeld", driverview, enabled=(WEBCAM or not PC)),
 
   PythonProcess("sensord", "system.sensord.sensord", only_onroad, enabled=not PC),
-  NativeProcess("ui", "selfdrive/ui", ["./ui"], always_run, enabled=(params0.get_bool("RaylibMode") == False), watchdog_max_dt=(5 if not PC else None)),
-  PythonProcess("raylib_ui", "selfdrive.ui.ui", always_run, enabled=(params0.get_bool("RaylibMode") == True), watchdog_max_dt=(5 if not PC else None)),
+  #NativeProcess("ui", "selfdrive/ui", ["./ui"], always_run, enabled=(params0.get_bool("RaylibMode") == False), watchdog_max_dt=(5 if not PC else None)),
+  PythonProcess("raylib_ui", "selfdrive.ui.ui", always_run, enabled=True, watchdog_max_dt=(5 if not PC else None)),
   PythonProcess("soundd", "selfdrive.ui.soundd", only_onroad),
   PythonProcess("locationd", "selfdrive.locationd.locationd", only_onroad),
   NativeProcess("_pandad", "selfdrive/pandad", ["./pandad"], always_run, enabled=False),
