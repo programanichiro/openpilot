671124a23758bf3d50f49dc84f50d95db8e27b83
ichiroMax <cxk01765@nifty.com>
2025-10-22 12:09:26 +0900

    self.limit_speed_override = True #テスト

--- selfdrive/ui/onroad/hud_renderer.py
+++ selfdrive/ui/onroad/hud_renderer.py
@@ -203,6 +203,7 @@ class HudRenderer(Widget):
     rect_color = COLORS.black_translucent
     rect_border_color = COLORS.border_translucent
 
+    self.limit_speed_override = True #テスト
     if self.limit_speed_override:
       # self.limit_speed_num = int(limitspeed_data[0])
       rect_color = rl.Color(235, 235, 235, 200)
