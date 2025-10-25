0365d8aca6d5e3c84565d189c5e44c933ac5f7b9
ichiroMax <cxk01765@nifty.com>
2025-10-25 15:32:28 +0900

    ★1ドット下げ

--- selfdrive/ui/onroad/hud_renderer.py
+++ selfdrive/ui/onroad/hud_renderer.py
@@ -488,7 +488,7 @@ class HudRenderer(Widget):
     rl.draw_rectangle_rounded(temp_rc, 1.0, 10, temp_col)
 
     self._drawText(font=self._font_semi_bold,font_size=44,x=rect.x+65+120-5+40,y=temp_rc.y+temp_rc.height-1,text=self.temp_disp3,alpha=-1,color_ex=letter_col) #x,yを下段中心にtextを表示する
-    self._drawText(font=self._font_JP,font_size=56,x=rect.x+65+55+5-5+30,y=temp_rc.y+temp_rc.height+1,text=self.temp_disp2,alpha=-1,color_ex=letter_col) #x,yを下段中心にtextを表示する
+    self._drawText(font=self._font_JP,font_size=56,x=rect.x+65+55+5-5+30,y=temp_rc.y+temp_rc.height+2,text=self.temp_disp2,alpha=-1,color_ex=letter_col) #x,yを下段中心にtextを表示する
     self._drawText(font=self._font_JP,font_size=48,x=rect.x+65+5+5+20,y=temp_rc.y+temp_rc.height-1,text=self.temp_disp1,alpha=-1,color_ex=letter_col) #x,yを下段中心にtextを表示する
 
     calib_h = -33 -33 - 30; #表示位置を上に
