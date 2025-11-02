c3fe80ebeddf5662da616e7fff030d6750f96fba
ichiroMax <cxk01765@nifty.com>
2025-11-02 11:54:57 +0900

    文字と合わせた見栄えは？

--- selfdrive/ui/onroad/hud_renderer.py
+++ selfdrive/ui/onroad/hud_renderer.py
@@ -724,8 +724,9 @@ class HudRenderer(Widget):
           else:
             self.disp_ichiro_logo = True #速度ゼロの---は表示しない。(road_info_baeringは利用するのでroad_info_txt_flagはtrueとする。)
 
-    self._drawTextRight(self._font_uni, 44 , rect.x+rect.width-10, rect.y+rect.height - 5 , "テスト神奈川県茅ヶ崎市道路情報(12345)", 220)
-    self._drawTextRight(self._font_uni, 44 , rect.x+rect.width-10-1, rect.y+rect.height - 5 , "テスト神奈川県茅ヶ崎市道路情報(12345)", 220)
+    next_x = self._drawTextRight(self._font_uni, 44 , rect.x+rect.width-10, rect.y+rect.height - 2 , "テスト神奈川県茅ヶ崎市道路情報(12345)", 220)
+    self._drawTextRight(self._font_uni, 44 , rect.x+rect.width-10-1, rect.y+rect.height - 2 , "テスト神奈川県茅ヶ崎市道路情報(12345)", 220)
+    self._drawTextRight(self._font_semi_bold, 33, next_x-4, rect.y+rect.height - 5 , self.kmh , 255 , False , 0x24, 0x57, 0xa1 , 255,255,255,200 , 0 , 0.2 , 2 , -1)
 
   def _ip_update_state(self,sm):
     try:
