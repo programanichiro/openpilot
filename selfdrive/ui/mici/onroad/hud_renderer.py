17faf17935f75a2cb4a18f1f3cfe7850bfc2b55e
ichiroMax <cxk01765@nifty.com>
2025-12-05 17:23:40 +0900

    こう？

--- selfdrive/ui/mici/onroad/hud_renderer.py
+++ selfdrive/ui/mici/onroad/hud_renderer.py
@@ -254,7 +254,7 @@ class HudRenderer(Widget):
     """Draw the MAX speed indicator box."""
     alpha = self._set_speed_alpha_filter.update(0 < rl.get_time() - self._set_speed_changed_time < SET_SPEED_PERSISTENCE and
                                                 self._can_draw_top_icons and self._engaged)
-    if self._can_draw_top_icons: #standstill状態でも表示したいから、self._engagedを除く。
+    if self._engaged: #standstill状態でも表示したい。
       alpha = 1.0
     else:
       alpha = 0.0
