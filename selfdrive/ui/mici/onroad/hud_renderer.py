02185b8b9fda76ed78684f737b4def5284de2a30
ichiroMax <cxk01765@nifty.com>
2025-12-07 11:37:09 +0900

    中心の扱いが謎

--- selfdrive/ui/mici/onroad/hud_renderer.py
+++ selfdrive/ui/mici/onroad/hud_renderer.py
@@ -251,12 +251,12 @@ class HudRenderer(Widget):
 
     if self.steer_always:
       if self.cruise_available:
-        center = rl.Vector2(
-            dest_rect.x + dest_rect.width / 2,
-            dest_rect.y + dest_rect.height / 2
+        center = rl.Vector2( #センター？
+            dest_rect.x,# + dest_rect.width / 2,
+            dest_rect.y #+ dest_rect.height / 2
         )
         radius = dest_rect.width / 2  # 正円なら width/2 でOK
-        rl.draw_circle(int(center.x), int(center.y), int(radius), rl.Color(0x17, 0x86, 0x44, 224))
+        rl.draw_circle_v(center, radius, rl.Color(0x17, 0x86, 0x44, 224))
 
     # color and draw
     color = rl.Color(255, 255, 255, int(self._wheel_alpha_filter.x))
