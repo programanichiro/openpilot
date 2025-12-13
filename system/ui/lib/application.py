ef185dcaed5b2524d6343a8ceec974a0a290a79f
ichiroMax <cxk01765@nifty.com>
2025-12-13 09:24:15 +0900

    44pixleバイリニアで運用してみる。

--- system/ui/lib/application.py
+++ system/ui/lib/application.py
@@ -626,7 +626,7 @@ class GuiApplication:
 
     # 22ピクセルフォントを生成
     jp2_font = rl.load_font_ex(jp2_font_path, int(22*self._scale), jp2_codepoints, jp2_codepoint_count[0])
-    rl.set_texture_filter(jp2_font.texture, rl.TextureFilter.TEXTURE_FILTER_POINT)
+    rl.set_texture_filter(jp2_font.texture, rl.TextureFilter.TEXTURE_FILTER_BILINEAR)
     self._fonts["JP2"] = jp2_font
     self._font_path[jp2_font] = jp2_font_path
     rl.unload_codepoints(jp2_codepoints)
