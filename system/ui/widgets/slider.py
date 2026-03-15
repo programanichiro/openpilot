e2bfb602c505c8521a26600c6a77407669713aea
ichiroMax <cxk01765@nifty.com>
2026-03-15 10:13:46 +0900

    スライドボタンのbig_uiの位置検証。

--- system/ui/widgets/slider.py
+++ system/ui/widgets/slider.py
@@ -41,7 +41,7 @@ class SliderBase(Widget, abc.ABC):
     self._is_dragging_circle = False
 
     self._label = self._child(UnifiedLabel(title, font_size=36, font_weight=FontWeight.SEMI_BOLD, text_color=rl.WHITE,
-                                           alignment=rl.GuiTextAlignment.TEXT_ALIGN_RIGHT,
+                                           alignment=(rl.GuiTextAlignment.TEXT_ALIGN_RIGHT if gui_app.big_ui() == False else rl.GuiTextAlignment.TEXT_ALIGN_CENTER),
                                            alignment_vertical=rl.GuiTextAlignmentVertical.TEXT_ALIGN_MIDDLE, line_height=0.9, shimmer=True))
 
   @abc.abstractmethod
