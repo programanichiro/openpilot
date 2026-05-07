19afaa8f339cc0e9aab1bb8963bc89cb43eb57af
ichiroMax <cxk01765@nifty.com>
2026-05-07 21:36:27 +0900

    もう少し広く

--- selfdrive/ui/mici/layouts/home.py
+++ selfdrive/ui/mici/layouts/home.py
@@ -128,7 +128,7 @@ class NetworkIcon(Widget):
 class ThermalTextIcon(Widget):
   def __init__(self):
     super().__init__()
-    self.set_rect(rl.Rectangle(0, 0, 75, 44))  # max size of all icons
+    self.set_rect(rl.Rectangle(0, 0, 80, 44))  # max size of all icons
     self.temp_str = "°C"
     self._font_bold: rl.Font = gui_app.font(FontWeight.BOLD)
     self.warning_color = rl.Color(255, 255, 255, int(255 * 0.9))
