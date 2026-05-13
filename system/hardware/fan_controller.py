f891f55e940cccfcaa089cc99f37ead56badbb12
ichiroMax <cxk01765@nifty.com>
2026-05-13 20:19:34 +0900

    これも？

--- system/hardware/fan_controller.py
+++ system/hardware/fan_controller.py
@@ -232,7 +232,7 @@ class FanController:
 
       key = (tx, ty)
 
-      if self._current_tile == key:
+      if self._current_tile == key and self._current_conn != None:
           return True
 
       conn = self.get_conn(tx, ty)
