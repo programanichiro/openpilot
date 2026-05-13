83e2b7e2a6c33b1c645483a355ac5065204152f2
ichiroMax <cxk01765@nifty.com>
2026-05-13 16:54:53 +0900

    ダメっぽいね

--- system/hardware/fan_controller.py
+++ system/hardware/fan_controller.py
@@ -12,8 +12,6 @@ from openpilot.common.pid import PIDController
 
 SERVERS = [
   "http://overpass.kumi.systems/api/interpreter",
-  "http://overpass.kumi.systems/api/interpreter",
-  "http://overpass.kumi.systems/api/interpreter",
     # "https://overpass-api.de/api/interpreter",
     # "https://overpass.private.coffee/api/interpreter",
     # "https://maps.mail.ru/osm/tools/overpass/api/interpreter",
@@ -664,7 +662,7 @@ class FanController:
     #   fp.write('osm_fetch:%d, %.5f, %.5f' % (self.thread == None,self.latitude,self.longitude))
     if self.thread == None and (self.latitude != 0 or self.longitude != 0):
       try:
-        self.distance = 50 *2 * np.interp(self.velocity, [0, 50.0], [0.5, 1.0]) #検出範囲に速度を反映する。０〜50km/h -> 0.3〜1倍
+        self.distance = 50 * np.interp(self.velocity, [0, 50.0], [0.5, 1.0]) #検出範囲に速度を反映する。０〜50km/h -> 0.3〜1倍
         self_thread = threading.Thread(target=self.osm_fetch) #argsにselfは要らない。
         self.thread = self_thread
         #self_thread.setDaemon(True)
