b96a1986aeb7b8e15f1165c7d42cc0025b1f812a
ichiroMax <cxk01765@nifty.com>
2026-05-23 18:54:09 +0900

    self.params.put_nonblockingを廃止に対応忘れ。距離ボタンでsystemエラーを修正。

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -454,7 +454,7 @@ class SelfdriveD:
     if self.CP.openpilotLongitudinalControl:
       if any(not be.pressed and be.type == ButtonType.gapAdjustCruise for be in CS.buttonEvents):
         self.personality = (self.personality - 1) % 3
-        self.params.put_nonblocking('LongitudinalPersonality', self.personality)
+        self.params.put('LongitudinalPersonality', self.personality)
         # self.events.add(EventName.personalityChanged)イチロウパイロットでは要らない。
 
   def data_sample(self):
