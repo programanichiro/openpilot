9754443745f2204187f3d9dcf58aab9b7ea5ab7d
ichiroMax <cxk01765@nifty.com>
2026-06-13 16:07:00 +0900

    一応逆を試す。

--- selfdrive/modeld/dmonitoringmodeld.py
+++ selfdrive/modeld/dmonitoringmodeld.py
@@ -133,7 +133,7 @@ def main():
     if model_transform is None:
       cam = _os_fisheye if buf.width == _os_fisheye.width else _ar_ox_fisheye
       K = cam.intrinsics.copy()
-      K[0,2] -= 90 #デバイスが右に12cmくらいオフセットされていたら
+      K[0,2] += 90 #デバイスが右に12cmくらいオフセットされていたら
       model_transform = np.linalg.inv(np.dot(dmonitoringmodel_intrinsics, np.linalg.inv(K))).astype(np.float32)
 
     sm.update(0)
