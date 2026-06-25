9b3c6874ab020fe8f44a5e92318bdfd8fb7cf68d
ichiroMax <cxk01765@nifty.com>
2026-06-25 11:13:55 +0900

    Revert "bbbb6までは来ている。"
    This reverts commit 13869224934eecfed1320cab6f90bbc1a37893ba.

--- openpilot/system/hardware/fan_controller.py
+++ openpilot/system/hardware/fan_controller.py
@@ -1624,19 +1624,15 @@ def gps_local_write(latitude, longitude, bearing, velocity, timestamp):
 push_thread = None
 
 def gpslog_push_commit_fetch():
-  print("dddd1")
   global push_thread
   def run(cmd):
     return subprocess.run(cmd, cwd=GPS_DIR0, text=True, capture_output=True)
   # pull
   pull = run(["git", "pull"])
   if pull.returncode != 0:
-    print("dddd2")
     push_thread = None
     return #ネット未接続
-  print("dddd3")
   shutil.copy2("/data/openpilot/viewer.html", GPS_DIR0+"/viewer.html") #viewer.html更新
-  print("dddd4")
   run(["git", "commit", "-m", "gps"])
   # 未push確認
   check = run(["git", "rev-list", "@{u}..HEAD"]) # @{u}はorigin/mainになる
@@ -1665,24 +1661,19 @@ def gpslog_push_fetch():
   push_thread = None
 
 def gpslog_push():
-  print("cccc1")
   global push_thread
   if push_thread:
     return
-  print("cccc2")
   def run(cmd):
     return subprocess.run(cmd, cwd=GPS_DIR0, text=True, capture_output=True)
   # 変更があるならコミット作成
   status = run(["git", "status", "--porcelain"])
-  print("cccc3")
   if status.stdout.strip():
-    print("cccc4")
     run(["git", "add", "."]) #addはワーカスレッドには出さない。
     tmp_thread = threading.Thread(target=gpslog_push_commit_fetch)
     push_thread = tmp_thread
     tmp_thread.start()
   else:
-    print("cccc5")
     tmp_thread = threading.Thread(target=gpslog_push_fetch)
     push_thread = tmp_thread
     tmp_thread.start()
