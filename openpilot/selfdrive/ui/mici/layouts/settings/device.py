970b365777b22de6094bba11b9bc70d6b7ba5503
ichiroMax <cxk01765@nifty.com>
2026-06-30 12:03:04 +0900

    /data/fixed_fingerprint.txt'操作。車名の書き込みおよびautoで削除処理。

--- openpilot/selfdrive/ui/mici/layouts/settings/device.py
+++ openpilot/selfdrive/ui/mici/layouts/settings/device.py
@@ -308,6 +308,10 @@ class VehicleSelectMici3(NavScroller):
 
   def year_select_callback(self, year_model):
     #ここでyear_modelを受け取り、self._makerとself._car_nameと結合して/data/fixed_fingerprint.txtに保存する。
+    with open('/data/fixed_fingerprint.txt','r') as fp:
+      fp.write(f"{self._maker} {self._car_name} {year_model}")
+    gui_app.pop_widget()
+    gui_app.pop_widget()
     gui_app.pop_widget()
 
 
@@ -332,6 +336,9 @@ class VehicleSelectMici2(NavScroller):
 
   def vehicle_select_callback(self, car_name):
     #ここでcar_nameを受け取り、self._makerと結合して/data/fixed_fingerprint.txtに保存する。
+    with open('/data/fixed_fingerprint.txt','r') as fp:
+      fp.write(f"{self._maker} {car_name}")
+    gui_app.pop_widget()
     gui_app.pop_widget()
 
 
@@ -340,9 +347,10 @@ class VehicleSelectMici(NavScroller):
     super().__init__()
 
     def vehicle_auto_select_callback():
+      os.remove('/data/fixed_fingerprint.txt')
       gui_app.pop_widget()
 
-    vehicle_auto_select = BigButton("suto select", "")
+    vehicle_auto_select = BigButton("auto select", "")
     vehicle_auto_select.set_click_callback(vehicle_auto_select_callback)
 
     self._scroller.add_widgets([
